e8d6c8f21f67e2adad241be34b818769d552dd57
Vitor Freitas <vitorfs@gmail.com>
2013-05-15 10:28:30 -0300

    Initial commit

---
+++ maximum-matching.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  maximum-matching.c
+ *
+ *    Description:  Maximum Matching
+ *
+ *        Version:  1.0
+ *        Created:  15/05/2013 10:26:38
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Vitor Freitas (vfs), vitorfs@gmail.com
+ *                  Rodrigo Martins (rfm), drg.martins@gmail.com
+ *        Company:  Universidade Federal de Juiz de Fora (UFJF)
+ *
+ * =====================================================================================
+ */
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
