cebbe941be6b48a515029eb075b7a888dfa185e5
Vitor Freitas <vitorfs@gmail.com>
2013-06-02 02:38:27 -0300

    adjusting grafo3 and maximum matching function

--- maximum-matching.c
+++ maximum-matching.c
@@ -124,14 +124,24 @@ Graph* maximal_matching(Graph* g) {
     if (saturated[v1] == 0) {
       adj = get_adjacency(m, v1);
       if (adj[0] > 0) {
-        v2 = adj[1];
-         
-        insert_arc(matching, v1, v2, m->arcs[v1][v2]);
-        remove_arc(m, v1, v2);
+        
+        int no_adj_available = 0;
 
-        free(adj);
-        saturated[v1] = 1;
-        saturated[v2] = 1;
+        for (j = 1 ; j <= adj[0] ; j++) {
+          if (saturated[adj[j]] == 0) {
+            v2 = adj[j];
+            no_adj_available = 1;
+          }
+        }
+        
+        if (no_adj_available == 1) {
+          insert_arc(matching, v1, v2, m->arcs[v1][v2]);
+          remove_arc(m, v1, v2);
+          free(adj);
+          saturated[v1] = 1;
+          saturated[v2] = 1;
+        }
+
       }
     }
   }
